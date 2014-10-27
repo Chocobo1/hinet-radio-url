bd746c75917f86b5cd0ec13daccc7a6ed2d04e9a
Chocobo1 <Chocobo1@users.noreply.github.com>
2014-10-27 16:28:25 +0800

    encapsulate into a function for easy use

--- run.sh
+++ run.sh
@@ -2,20 +2,30 @@
 # Copyright (C) 2014  Mike Tzou
 # This script is licensed under GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
 
-# variables
+function hinet_radio_url()
+{
+	# $1=(input)station ID, $2=(ouput)variable for returning the result URL
+
+	local SED=/usr/bin/sed
+	local ECHO=/usr/bin/echo
+	local GREP=/usr/bin/grep
+	local WGET=/usr/bin/wget
+
+	local BASE_URL="http://hichannel.hinet.net/radio/mobile/index.do?id=$1"
+	local URL1="$($WGET -q -t 3 -O - "$BASE_URL" | $GREP -Po "'\K.+token1.+token2.+?(?=')" | $SED 's/\\//g')"
+	local URL2="$($WGET -q -t 3 -O - $URL1 | $GREP -Po "\K.+token1.+token2.+" | $SED 's/-video=0//g' )"
+	local URL3="$($ECHO "$URL1" | $SED 's/index.m3u8.*$//g')"$URL2
+	eval $2=\$URL3 
+}
+
+# example
 RADIO_ID="228"
-BASE_URL="http://hichannel.hinet.net/radio/mobile/index.do?id=$RADIO_ID"
+URL=''
+hinet_radio_url $RADIO_ID URL
 
-SED=/usr/bin/sed
+# output
 ECHO=/usr/bin/echo
-GREP=/usr/bin/grep
-WGET=/usr/bin/wget
-
-URL1="$($WGET -q -t 3 -O - "$BASE_URL" | $GREP -Po "'\K.+token1.+token2.+?(?=')" | $SED 's/\\//g')"
-URL2="$($WGET -q -t 3 -O - $URL1 | $GREP -Po "\K.+token1.+token2.+" | $SED 's/-video=0//g' )"
-URL3="$($ECHO "$URL1" | $SED 's/index.m3u8.*$//g')"$URL2
-
 $ECHO
 $ECHO "Radio ID: $RADIO_ID"
 $ECHO
-$ECHO "URL: $URL3"
+$ECHO "URL: $URL"
