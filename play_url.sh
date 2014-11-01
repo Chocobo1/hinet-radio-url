bb6c8c99ec24655b289ac3f826023160d88e69f9
Chocobo1 <Chocobo1@users.noreply.github.com>
2014-11-01 16:45:56 +0800

    add a loop when wget fails cleanup

--- play_url.sh
+++ play_url.sh
@@ -9,13 +9,16 @@ function hinet_radio_url()
 	local SED=/usr/bin/sed
 	local ECHO=/usr/bin/echo
 	local GREP=/usr/bin/grep
-	local WGET=/usr/bin/wget
+	local WGET='/usr/bin/wget -q -t 3 -O -'
 
 	local BASE_URL="http://hichannel.hinet.net/radio/mobile/index.do?id=$1"
-	local URL1="$($WGET -q -t 3 -O - "$BASE_URL" | $GREP -Po "'\K.+token1.+token2.+?(?=')" | $SED 's/\\//g')"
-	local URL2="$($WGET -q -t 3 -O - $URL1 | $GREP -Po "\K.+token1.+token2.+" | $SED 's/-video=0//g' )"
-	local URL3="$($ECHO "$URL1" | $SED 's/index.m3u8.*$//g')"$URL2
-	eval $2=\$URL3 
+	local URL1="$($WGET "$BASE_URL" | $GREP -Po "'\K.+token1.+token2.+?(?=')" | $SED 's/\\//g')"
+	local URL2=''
+	while [ -z $URL2 ]; do
+		URL2="$($WGET "$URL1" | $GREP -Po "\K.+token1.+token2.+" | $SED 's/-video=0//g' )"
+	done
+	local URL3="$($ECHO "$URL1" | $SED 's/index.m3u8.*$//g')""$URL2"
+	eval $2=\$URL3
 }
 
 # example
