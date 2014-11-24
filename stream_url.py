b1bfc01bd466bf59666988b29be284540487a451
Chocobo1 <Chocobo1@users.noreply.github.com>
2014-11-24 12:49:36 +0800

    follow EAFP idiom

--- stream_url.py
+++ stream_url.py
@@ -4,28 +4,23 @@ Copyright (C) 2014 Mike Tzou
 This script is licensed under GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
 """
 
-import re , urllib , numbers
+import re , urllib
 
 def hinet_radio_stream_url( id ):
 	"""
 	return HLS url for radio station `id`
-	return empty string when error occurs
 
 	arguments:
-	id (integer) -- should be valid radio station ID
+	id -- should be valid radio station ID
 	"""
-
-	# naive input checking
-	if not isinstance( id , numbers.Integral ):
-		return ""
 
 	base_url = "http://hichannel.hinet.net/radio/mobile/index.do?id=" + str( id )
 	base_url_data = urllib.urlopen( base_url ).read()
 
-	url1 = re.search( "(?<=').+token1.+token2.+?(?=')" , base_url_data )
-	if url1 == None:
-		return ""
-	url1 = url1.group()
+	try:
+		url1 = re.search( "(?<=').+token1.+token2.+?(?=')" , base_url_data ).group()
+	except AttributeError:
+		raise IOError( "variable `id` invalid" )
 	url1 = url1.replace( "\\" , "" )
 
 	url2 = urllib.urlopen( url1 ).read()
@@ -43,5 +38,7 @@ if __name__ == "__main__":
 		id = [228]
 
 	for i in id:
-		j = int( i )
-		print ( "\nStation ID: %d\n\nStream URL: %s\n" ) % ( j , hinet_radio_stream_url( j ) )
+		try:
+			print ( "\nStation ID: %s\n\nStream URL: %s\n" ) % ( str( i ) , hinet_radio_stream_url( i ) )
+		except IOError:
+			pass
