accb6c0709c1cbbcb26cfa8aae84fa5980d5a0e6
Chocobo1 <Chocobo1@users.noreply.github.com>
2015-02-03 18:16:09 +0800

    better/alternate approach

--- station_id.sh
+++ station_id.sh
@@ -7,16 +7,31 @@ function hinetRadioStationId()
 	local GREP=/usr/bin/grep
 	local SORT=/usr/bin/sort
 	local WGET='/usr/bin/wget -q -t 3 -O -'
-	local base_url="http://hichannel.hinet.net/radio/index.do"
 
-	local data="$($WGET "$base_url")"
-	IFS=$'\n'
-	local id_list=( $( $ECHO $data | $GREP -Po "(?<=onclick=\"indexUtil.setRadioDetail\(').+?(?=')" ) )
-	local name_list=( $( $ECHO $data | $GREP -Po '(?<=\t\t\t\t\t\t\t\t\t\t<p>).+?(?=<)' ) )
+	local base_url='http://hichannel.hinet.net/radio/channelList.do?pN='
+
+	local id_list
+	local name_list
+	local current_page=1
+	local total_pages=2
+	while [ $current_page -le $total_pages ]; do
+		local data="$($WGET "$base_url$current_page")"
+
+		if [ $current_page -eq 1 ]; then
+			total_pages=( $( $ECHO $data | $GREP -Po '(?<=\"pageSize\":).+?(?=,)' ) )
+		fi
+		current_page=$((current_page + 1))
+
+		local ORIG_IFS=$IFS
+		IFS=$'\n'
+		id_list+=( $( $ECHO $data | $GREP -Po '(?<=\"channel_id\":\").+?(?=\")' ) )
+		name_list+=( $( $ECHO $data | $GREP -Po '(?<=\"channel_title\":\").+?(?=\")' ) )
+		IFS=$ORIG_IFS
+	done
 
 	for i in "${!name_list[@]}"
 	do
-		$ECHO "${id_list[$i+1]} - ${name_list[$i]}"
+		$ECHO "${id_list[$i]} - ${name_list[$i]}"
 	done | $SORT -n -k1
 }
 
